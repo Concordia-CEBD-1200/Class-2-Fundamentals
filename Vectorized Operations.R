ba06c7b6555c1f3eeda4597ace3bbb304837acd0
Pranav Bhatia <30525912+PranavBhatia@users.noreply.github.com>
2022-06-27 09:10:49 -0400

    Update Vectorized Operations.R

--- Vectorized Operations.R
+++ Vectorized Operations.R
@@ -1,6 +1,34 @@
-c(1, 100) > c(100, 1)
+x <- rnorm(5)
+x
 
-a <- c(1, 5, 10, 22, 6)
-b <- c(10, 100, 1000, 10000)
+# R specific programming loop
+for(i in x) {
+  print(i)
+}
 
-a+b # Recycling of vectors
+print(x[1])
+print(x[2])
+print(x[3])
+print(x[4])
+print(x[5])
+
+# Conventional programming loop
+for (j in 1:5) {
+  print(x[j]) 
+}
+
+# -----------------------------------------------------------
+
+N <- 100000000
+a <- rnorm(N)
+b <- rnorm(N)
+
+# Vectorized approach
+c <- a * b
+
+# De-Vectorized approach
+d <- rep(NA, N)
+
+for(i in 1:N) {
+  d[i] <- a[i] * b[i]  
+}
